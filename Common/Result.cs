549b0045fb6aa06277281591bee69db7049e48e4
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-03-24 16:57:56 +0100

    result pattern

---
+++ Common/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace apbd_cw2_s33333.Common;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public string ErrorMessage { get; }
+    
+    private Result(bool isSuccess, string errorMessage)
+    {
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result Success() => new Result(true, string.Empty);
+    public static Result Failure(string message) => new Result(false, message);
+}
