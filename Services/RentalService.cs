c8e3f3afb0e8c153c9898757a2b472d3aac8e51c
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-03-24 17:17:39 +0100

    added rental service class w/ renting logic

---
+++ Services/RentalService.cs
@@ -0,0 +1,37 @@
+﻿using apbd_cw2_s33333.Common;
+using apbd_cw2_s33333.Interface;
+using apbd_cw2_s33333.Models;
+
+namespace apbd_cw2_s33333.Services;
+
+public class RentalService
+{
+    private readonly List<Equipment> _equipments = new();
+    private readonly List<User> _users = new();
+    private readonly List<Rental> _rentals = new();
+    private readonly IPenaltyCalc _penaltyCalc;
+
+    public RentalService(IPenaltyCalc penaltyCalc)
+    {
+        _penaltyCalc = penaltyCalc;
+    }
+    
+    public void AddUser(User user) =>  _users.Add(user);
+    public void AddEquipment(Equipment equipment) =>  _equipments.Add(equipment);
+
+    public Result RentEquipment(User user, Equipment equipment, int daysToRent)
+    {
+        if (equipment.Status != EquipmentStatus.Available)
+            return Result.Failure($"Item '{equipment.Name}' jest niedostepny.");
+
+        int activeRentalsCount = _rentals.Count(r => r.User.Id == user.Id && r.isActive);
+        if (activeRentalsCount >= user.MaxRentals)
+            return Result.Failure($"User '{user.Firstname}' osiagnal limit wypozyczen");
+
+        equipment.Status = EquipmentStatus.Rented;
+        var rental = new Rental(user, equipment, daysToRent);
+        _rentals.Add(rental);
+        
+        return Result.Success();
+    }
+}
