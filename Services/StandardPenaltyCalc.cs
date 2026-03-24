c31044f95463db3924a54c8e2557665860797b71
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-03-24 17:05:51 +0100

    added full penalty calculator

---
+++ Services/StandardPenaltyCalc.cs
@@ -0,0 +1,16 @@
+﻿using apbd_cw2_s33333.Interface;
+
+namespace apbd_cw2_s33333.Services;
+
+public class StandardPenaltyCalc : IPenaltyCalc
+{
+    private const decimal DailyPenaltyRate = 10.0m;
+
+    public decimal CalculatePenalty(DateTime dueDate, DateTime actualReturnDate)
+    {
+        if (actualReturnDate <= dueDate) return 0;
+
+        var delay = (actualReturnDate - dueDate).Days;
+        return delay * DailyPenaltyRate;
+    }
+}
