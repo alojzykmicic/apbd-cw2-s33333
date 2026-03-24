e07a66f555c15698ce3be41ab47daa54ce7c0e9f
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-03-24 16:18:49 +0100

    Added single rent model

---
+++ Models/Rental.cs
@@ -0,0 +1,28 @@
+﻿namespace apbd_cw2_s33333.Models;
+
+public class Rental
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime RentDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; private set; }
+    public decimal PenaltyFee { get; private set; } = 0;
+
+    public bool isActive => ReturnDate == null;
+
+    public Rental(User user, Equipment equipment, int daysToRent)
+    {
+        User = user;
+        Equipment = equipment;
+        RentDate = DateTime.Now;
+        DueDate = DateTime.Now.AddDays(daysToRent);
+    }
+
+    public void MarkAsReturned(DateTime returnDate, decimal penalty)
+    {
+        ReturnDate = returnDate;
+        PenaltyFee = penalty;
+    }
+}
