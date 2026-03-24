5ebcdd44007caab3792154ef7a71ea2d7c761945
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-03-24 16:05:53 +0100

    Added user models: student, employee

---
+++ Models/Employee.cs
@@ -0,0 +1,7 @@
+﻿namespace apbd_cw2_s33333.Models;
+
+public class Employee : User
+{
+    public override int MaxRentals => 5;
+    public Employee(string firstname, string lastname) : base(firstname, lastname) { }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace apbd_cw2_s33333.Models;
+
+public class Student : User
+{
+    public override int MaxRentals => 2;
+
+    public Student(string firstname, string lastname) : base(firstname, lastname) { }
+}
